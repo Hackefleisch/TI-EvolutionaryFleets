5fdae741d4c465ae7c3a90163e0cbde27bdc4178
eisenhuth451 <eisenhuth451@gmail.com>
2019-08-06 12:38:12 +0200

    Added parameters

--- TI-EvolutionaryFleets/Simulation.cpp
+++ TI-EvolutionaryFleets/Simulation.cpp
@@ -5,12 +5,12 @@
 void Simulation::Initialize(){
 
 	for(Fleet& fleet : fleets){
-		fleet.Initialize(9, 3);
+		fleet.Initialize(startingRessources, startingFleetSize);
 		std::cout << fleet.GetName() << std::endl;
 	}
 
-	fleets.at(0).Fight(fleets.at(1));
-	fleets.at(2).Fight(fleets.at(3));
+	fleets.at(0).Fight(fleets.at(1), true);
+	fleets.at(2).Fight(fleets.at(3), false);
 
 }
 
@@ -20,7 +20,8 @@ Simulation::Simulation()
 	d10(1, 10),
 	d1000(1, 1000)
 {
-	for(int i = 0; i < 5; i++){
+	fleets.reserve(populationSize);
+	for(int i = 0; i < populationSize; i++){
 		fleets.emplace_back(&rng, &d1000, &d10);
 	}
 }
--- TI-EvolutionaryFleets/Simulation.h
+++ TI-EvolutionaryFleets/Simulation.h
@@ -16,5 +16,25 @@ private:
 	std::mt19937 rng;
 	std::uniform_int_distribution<int> d10;
 	std::uniform_int_distribution<int> d1000;
+	// parameters
+	float startingRessources = 9.0f;
+	int startingFleetSize = 3;
+	float maxRessources = 40.0f;
+	int maxFleetSize = 10;
+	int populationSize = 1000;
+	int maxIterations = 10000;
+	float minFitnessImprovement = 0.0005f;
+	float winScoreFactor = 1.0f;
+	float dstTotalScoreFactor = 1.0f;
+	float tknTotalScoreFactor = 1.0f;
+	float dstRelScoreFactor = 1.0f;
+	float tknRelScoreFactor = 1.0f;
+	float frctScoreFactor = 1.0f;
+	float mutationRate = 0.1f;
+	float mutationIntensity = 1.0f;
+	int nScoringEncounters = 5;
+	int nEncounterRepitions = 5;
+	bool randomizedDeath = true;
+	float deathRate = 0.5f;
 };
 
